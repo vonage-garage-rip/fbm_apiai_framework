c428c5545e0bf73039ccfee549f05f9d23815e1a
Tony Hung <tbass134@gmail.com>
2017-10-03 14:48:47 -0400

    return full object to actions

--- sessionsManager.js
+++ sessionsManager.js
@@ -144,7 +144,7 @@ const handleApiaiResponse = (apiairesponse) => {
         console.log("HANDLE APIAI RESPONSE: ", apiairesponse);
         let actionName = apiairesponse.result.action
         if ( actionName && actionName!=="input.unknown" ) {
-            actionsManager.handleAction(apiairesponse.result.action, apiairesponse.result)
+            actionsManager.handleAction(apiairesponse.result.action, apiairesponse)
         }
         if (apiairesponse.result.fulfillment.data && apiairesponse.result.fulfillment.data.facebook) {
             fbChannel.sendMessageToUser({ type: MESSAGE_TYPES.CUSTOME, payload: { facebook: apiairesponse.result.fulfillment.data.facebook } }, apiairesponse.sessionId);
