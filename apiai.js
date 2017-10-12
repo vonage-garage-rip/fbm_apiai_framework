5ca28e32b4be7e363db47b00f3722179cc9229ca
tefrati <tzahi.efrati@gmail.com>
2017-10-12 13:44:01 -0400

    turned to class that can manage multiple agents

--- apiai.js
+++ apiai.js
@@ -1,65 +1,78 @@
 var apiai = require('apiai');
 const sessionsManager = require('./sessionsManager');
 
-var app = apiai(process.env.APIAI_TOKEN);
+var apiaiAgents ={}
 
-const sendTextMessageToApiAi = (textMessage, sessionId) => {
-    return new Promise(function(resolve, reject) {
-        let session = sessionsManager.getSessionBySessionId(sessionId);
-        let userProfileContext = {
-            "name": "user-profile",
-            "parameters": {
-                "full_name": session.profile.first_name + " " + session.profile.last_name,
-                "first_name": session.profile.first_nam,
-                "last_visit": session.lastInboundMessage.format()
-            },
-            "lifespan": 5
-        };
-        var request = app.textRequest(textMessage, {sessionId: sessionId, contexts: [userProfileContext], timezone: session.profile.timezone});
+class ApiAi {
+    constructor (apiaiToken) {
+        this.app = apiai(apiaiToken);
+    }
 
-        request.on('response', function(response) {
-            console.log("sendTextMessageToApiAi: response=" + JSON.stringify(response));
-            return resolve(response);
+    sendTextMessageToApiAi(textMessage, sessionId) {
+        return new Promise(function(resolve, reject) {
+            let session = sessionsManager.getSessionBySessionId(sessionId);
+            let userProfileContext = {
+                "name": "user-profile",
+                "parameters": {
+                    "full_name": session.profile.first_name + " " + session.profile.last_name,
+                    "first_name": session.profile.first_name,
+                    "last_visit": session.lastInboundMessage.format()
+                },
+                "lifespan": 5
+            };
+            var request = this.app.textRequest(textMessage, {sessionId: sessionId, contexts: [userProfileContext], timezone: session.profile.timezone});
+
+            request.on('response', function(response) {
+                console.log("sendTextMessageToApiAi: response=" + JSON.stringify(response));
+                return resolve(response);
+            });
+
+            request.on('error', function(error) {
+                return reject(error);
+            });
+
+            request.end();
         });
+    }
 
-        request.on('error', function(error) {
-            return reject(error);
+    sendEventToApiAi(event, sessionId){
+        return new Promise(function(resolve, reject) {
+            let session = sessionsManager.getSessionBySessionId(sessionId);
+            let userProfileContext = {
+                "name": "user-profile",
+                "parameters": {
+                    "full_name": session.profile.first_name + " " + session.profile.last_name,
+                    "first_name": session.profile.first_name,
+                    "last_visit": session.lastInboundMessage.format(),
+                },
+                "lifespan": 5
+            };
+            let eventArg = {
+                "name": event.type,
+                "data": event.data, 
+            };
+
+            var request = this.app.eventRequest(eventArg, {sessionId: sessionId, contexts: [userProfileContext]});
+
+            request.on('response', function(response) {
+                console.log("sendEventToApiAi: response=" + JSON.stringify(response));
+                return resolve(response);
+            });
+
+            request.on('error', function(error) {
+                return reject(error);
+            });
+
+            request.end();
         });
-
-        request.end();
-    });
+    }
 }
 
-const sendEventToApiAi = (event, sessionId) => {
-    return new Promise(function(resolve, reject) {
-        let session = sessionsManager.getSessionBySessionId(sessionId);
-        let userProfileContext = {
-            "name": "user-profile",
-            "parameters": {
-                "full_name": session.profile.first_name + " " + session.profile.last_name,
-                "first_name": session.profile.first_name,
-                "last_visit": session.lastInboundMessage.format(),
-            },
-            "lifespan": 5
-        };
-        let eventArg = {
-            "name": event.type,
-            "data": event.data, 
-        };
-
-        var request = app.eventRequest(eventArg, {sessionId: sessionId, contexts: [userProfileContext]});
-
-        request.on('response', function(response) {
-            console.log("sendEventToApiAi: response=" + JSON.stringify(response));
-            return resolve(response);
-        });
-
-        request.on('error', function(error) {
-            return reject(error);
-        });
-
-        request.end();
-    });
+const getAgent = (apiaiToken) => {
+    if (!apiaiAgents[apiaiToken]) {
+        apiaiAgents[apiaiToken] = new ApiAi(apiaiToken)
+    }
+    return apiaiAgents[apiaiToken]
 }
 
-module.exports = {sendTextMessageToApiAi, sendEventToApiAi};
+module.exports = {getAgent}
