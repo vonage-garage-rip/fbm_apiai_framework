09395d05d39f8aa820c9e8ce3f473b1b6c9b956f
Tzahi Efrati <zefrati@VONAGE.NET>
2017-10-16 14:41:47 -0400

    implemented sendCommentToPost

--- channels/facebook/wphook.js
+++ channels/facebook/wphook.js
@@ -6,6 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
+
 const sessionsMsessionsManageranager = require('../../sessionsManager');
 
 /* jshint node: true, devel: true */
@@ -89,7 +90,7 @@ function processPageEvents(data) {
 		// Chat messages sent to the page
     if(entry.messaging) {
       entry.messaging.forEach(function(messaging_event){
-        console.log('Page Messaging Event',page_id,messaging_event);
+        console.log('Page Messaging Event', page_id, messaging_event);
       });
     }
 		// Page related changes, or mentions of the page
@@ -98,11 +99,11 @@ function processPageEvents(data) {
         console.log('Page Change', page_id, change);
         let inboundMessage = {
           channel: sessionsMsessionsManageranager.CHANNELS.FB_WORKPLACE,
-          from: change.value.sender_id, /// Change to group ID
+          source: change.value.post_id, 
+          from: change.value.sender_name, //  perhaps this should be added to session.contexts
           to: page_id,
           text: change.value.message.substring(change.value.message.indexOf(' ')+1)
         };
-        /// TODO considerf adding conversation context here such as sales agent name
         sessionsMsessionsManageranager.handleInboundChannelMessage(inboundMessage)
       });
     }
@@ -131,27 +132,53 @@ function processWorkplaceSecurityEvents(data) {
   data.entry.forEach(function(entry){
     let group_id = entry.id;
     entry.changes.forEach(function(change){
-      console.log('Workplace Security Change',group_id,change);
+      console.log('Workplace Security Change', group_id, change);
     });
   });
 }
 
-function sendMessageToGroup(message, groupId) {
-  graphapi({
-    method: 'POST',
-    url: '/' + groupId + '/feed',
-    qs: {
-        'message': message
-    }
-  },function(error, response, body) {
-      if(error) {
-          console.error(error);
-      } else {
-          var post_id = JSON.parse(body).id;
-          console.log('sendMessageToGroup: Published message. Post ID= ' + post_id);
+function sendCommentToPost(message, postId) {
+  return new Promise((resolve, reject) => {
+    graphapi({
+      method: 'POST',
+      url: '/' + postId + '/comments',
+      qs: {
+          'message': message
       }
-  });
+    }, (error, response, body) => {
+        if(error) {
+            console.error("sendCommentToPost got an error: " + error);
+            return reject(error)
+        } else {
+            var commentId = JSON.parse(body).id;
+            console.log('sendCommentToPost: Published comment. comment ID= ' + commentId);
+            return resolve(commentId)
+        }
+    });
+  })
+}
+
+function sendMessageToGroup(message, groupId) {
+  return new Promise((resolve, reject) => {
+    graphapi({
+      method: 'POST',
+      url: '/' + groupId + '/feed',
+      qs: {
+          'message': message
+      }
+    }, (error, response, body) => {
+        if(error) {
+            console.error(error);
+            return reject(error)
+        } else {
+            var post_id = JSON.parse(body).id;
+            console.log('sendMessageToGroup: Published message. Post ID= ' + post_id);
+            return resolve(post_id)
+        }
+    });
+  })
 }
 
 module.exports.handleInboundEvent = handleInboundEvent
 module.exports.sendMessageToGroup = sendMessageToGroup
+module.exports.sendCommentToPost = sendCommentToPost
