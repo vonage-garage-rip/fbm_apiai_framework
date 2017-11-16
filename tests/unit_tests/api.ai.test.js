4229a2ccac02fc03a255e40498a57e1ead70f8e6
Kevin Alwell <alwellkevin44@gmail.com>
2017-11-16 13:00:01 -0500

    testing getAgent function. FIRST PASSING REAL UNIT TEST

--- tests/unit_tests/api.ai.test.js
+++ tests/unit_tests/api.ai.test.js
@@ -1,10 +1,8 @@
 //Loads all .env variables into PROCESS.ENV
 require('dotenv').config();
 //Main Target File TO TEST
-console.log("here");
 const sessionsManager = require("../../sessionsManager.js");
 const apiai = require("../../apiai.js");
-console.log("there", apiai);
 //Dependencies
 const session = require('./dependencies/session');
 const apiaiMsg = require('./dependencies/message');
@@ -13,15 +11,28 @@ const assert = require('assert');
 const nock = require('nock');
 
 describe('*****ApiAi Test Suite: ', function() {
-    beforeEach(() => {
-    nock('https://api.dialogflow.com/v1/')
-      .post('/query/?v=20150910')
-      .reply(200, apiaiMsg);
-  });
+    describe('Function: getAgent() ', function() {
+      var agent;
+        
+        before(() => {
+          agent = apiai.getAgent(process.env.APIAI_TOKEN);
+        });
 
-  describe('Function: sendTextMessageToApiAi() ', function() {
-    it('should get session, sendTextMessageToApiAi, then handleApiaiResponse ', function() {
-      // sessionsManager.handleInboundChannelPostback();
+        it('should get return an apiai agent', function() {
+          expect(agent.app.hostname).to.equal('api.api.ai');
+        });
     });
-  });
+
+
+    describe('Function: sendTextMessageToApiAi() ', function() {
+        before(() => {
+            nock('https://api.dialogflow.com/v1/')
+                .post('/query/?v=20150910')
+                .reply(200, apiaiMsg);
+        });
+
+        it('should get session, sendTextMessageToApiAi, then handleApiaiResponse ', function() {
+            // sessionsManager.handleInboundChannelPostback();
+        });
+    });
 });
